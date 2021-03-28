e728fa4681d8fdfc079c60df6db25f0540fb9d8a
unknown <hello.jewelahmed@gmail.com>
2021-03-28 14:09:02 +0600

    componetn with dynamic data

--- src/App.js
+++ src/App.js
@@ -7,33 +7,30 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Display></Display>
-        <Display></Display>
-        <Display></Display>
-        <Display></Display>
+        <Display name="Casual Back Pack" price="$34"></Display>
+        <Display name="Casual Shoe Mens" price="$24"></Display>
+        <Display name="Casual Shoe Girls" price="$14"></Display>
+        <Display name="Casual Shirts Mens" price="$54"></Display>
       </header>
     </div>
   );
 }
 
-function Display() {
+function Display(props) {
   const cssClass ={
-    backgroundColor:'gray',
-    color:'cyan',
-    margin: '10px',
+    borderRadius:'10px',
+    backgroundColor: 'gray',
+    color:'black',
+    margin:'10px',
     padding:'10px',
-    borderRadius:'10px'
-
   }
   return (
-    // Important Note: Component Mus Inside a closer Tag
     <div style={cssClass}>
-      <h1>Casual Back Pack</h1>
-      <h3>Price:$23</h3>
+      <h1>{props.name}</h1>
+      <h3>{props.price}</h3>
     </div>
   )
 }
-
 
 
 
