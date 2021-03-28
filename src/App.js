26f474966177412b2f7e41623e0ef04ca7e5c9b9
unknown <hello.jewelahmed@gmail.com>
2021-03-28 13:41:47 +0600

    component practicr

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit Done <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Display></Display>
+        <Display></Display>
+        <Display></Display>
+        <Display></Display>
       </header>
     </div>
   );
 }
+
+function Display() {
+  const cssClass ={
+    backgroundColor:'gray',
+    color:'cyan',
+    margin: '10px',
+    padding:'10px',
+    borderRadius:'10px'
+
+  }
+  return (
+    // Important Note: Component Mus Inside a closer Tag
+    <div style={cssClass}>
+      <h1>Casual Back Pack</h1>
+      <h3>Price:$23</h3>
+    </div>
+  )
+}
+
+
+
 
 export default App;
